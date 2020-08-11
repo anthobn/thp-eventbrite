b05fd128741f92df13ad4a61a595154b3a30cade
Anthony BARNOIN <contact@anthobn.fr>
2020-08-12 01:51:11 +0200

    test 4

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ApplicationMailer
 
+  default from: 'dorok92458@corsj.net'
+
   def welcome_new_user(user)
     @user = user
     @url = "http://localhost:3000"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,19 +59,12 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_LOGIN'],
-    :password => ENV['SENDGRID_PWD'],
-    :domain => 'murmuring-temple-89689.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true 
+  address:              'localhost',
+  port:                 1025,
+  domain:               'localhost' 
   }
-
-
 
 end
